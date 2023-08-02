bd77eaf3703d839f3a57753d4017e36247d6d0bf
Gandobilis <lashadeveloper@gmail.com>
2023-08-02 15:55:08 +0400

    Contact.vue view divided into components with appropriate composables.

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,9 +1,19 @@
 <script setup>
-import {ref} from "vue";
+import useContact from "@/composables/contact/useContact";
+import Info from "@/components/contact/Info.vue";
+import Form from "@/components/contact/Form.vue";
+import Map from "@/components/contact/Map.vue";
 
-const data = ref("Contact");
+const heading = useContact();
 </script>
 
 <template>
-  <p v-text="data"/>
+  <div class="max-w-7xl mt-20 mb-44 mx-auto space-y-14">
+    <h1 class="text-8xl font-medium leading-normal" v-text="heading"/>
+    <Info/>
+    <div class="grid grid-cols-2 gap-36">
+      <Form/>
+      <Map/>
+    </div>
+  </div>
 </template>
