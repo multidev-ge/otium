f63049782cc8cf8d58f5954a117f686dd3553a0e
Rikudo10 <giorgisamxaradze16@gmail.com>
2023-08-17 23:06:44 +0400

    Footer2

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <script setup>
 import useFooter from "@/composables/useFooter";
-const { FooterInfo, contactFormData,  submitContactForm, } = useFooter();
+const { FooterInfo, contactFormData, submitContactForm } = useFooter();
 </script>
 <template>
   <footer class="bg-[#000000] mt-12">
@@ -14,27 +14,41 @@ const { FooterInfo, contactFormData,  submitContactForm, } = useFooter();
         </div>
 
         <div class="px-4 w-[100%]">
-          <form @submit.prevent="submitContactForm" class="flex  md:gap-14 justify-between gap-4">
-            <div class="w-1/3	">
-                <label for="text" class="block  text-base	 font-medium text-[#FFFFFF99]">Your Name</label>
+          <form
+            @submit.prevent="submitContactForm"
+            class="flex md:gap-14 justify-between gap-4"
+          >
+            <div class="w-1/3">
+              <label
+                for="text"
+                class="block text-base font-medium text-[#FFFFFF99]"
+                >Your Name</label
+              >
               <input
                 v-model="contactFormData.name"
-                id="email"
+                id="text"
                 type="text"
-                class="border-[#FFFFFF4D] block w-full  bg-black outline-0 text-white border-b-2 text-lg"
+                class="border-[#FFFFFF4D] block w-full bg-black outline-0 text-white border-b-2 text-lg"
                 required
+                autocomplete="name"
               />
             </div>
-            <div class="w-1/3	">
-                <label  for="tel" class="block  text-base	 font-medium text-[#FFFFFF99]">Phone Number</label>
+            <div class="w-1/3">
+              <label
+                for="tel"
+                class="block text-base font-medium text-[#FFFFFF99]"
+                >Phone Number</label
+              >
               <input
                 v-model="contactFormData.phoneNumber"
                 id="tel"
                 type="tel"
-                class="border-[#FFFFFF4D] block w-full  bg-black outline-0 text-white border-b-2 text-lg"
+                class="border-[#FFFFFF4D] block w-full bg-black outline-0 text-white border-b-2 text-lg"
                 required
+                autocomplete="tel"
               />
             </div>
+
             <button
               type="submit"
               class="flex gap-2 pt-3 mt-1 px-14 text-base font-medium text-center text-[#221C32] rounded-xl bg-white sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
@@ -46,6 +60,97 @@ const { FooterInfo, contactFormData,  submitContactForm, } = useFooter();
               />
             </button>
           </form>
+          <div class="flex justify-between pt-16 pb-14">
+            <div class="w-1/3">
+              <div class="gap-2 flex">
+                <component
+                  class=""
+                  :is="FooterInfo[0].Footercomponents.icon3"
+                />
+                <p class="text-sm text-[#FFFFFF99]">
+                  {{ FooterInfo[0].Footercomponents.title1 }}
+                </p>
+              </div>
+              <p class="text-sm text-[#FFFFFF99] pt-3 w-44">
+                {{ FooterInfo[0].Footercomponents.address }}
+              </p>
+            </div>
+            <div class="w-1/3">
+              <div class="gap-2 flex">
+                <component
+                  class=""
+                  :is="FooterInfo[0].Footercomponents.icon2"
+                />
+                <p class="text-sm text-[#FFFFFF99]">
+                  {{ FooterInfo[0].Footercomponents.title2 }}
+                </p>
+              </div>
+              <p class="text-sm text-[#FFFFFF99] pt-3 w-44">
+                {{ FooterInfo[0].Footercomponents.number }}
+              </p>
+            </div>
+            <div class="w-1/5">
+              <div class="gap-2 flex">
+                <component
+                  class=""
+                  :is="FooterInfo[0].Footercomponents.icon1"
+                />
+                <p class="text-sm text-[#FFFFFF99]">
+                  {{ FooterInfo[0].Footercomponents.title3 }}
+                </p>
+              </div>
+              <p class="text-sm text-[#FFFFFF99] pt-3 w-44">
+                {{ FooterInfo[0].Footercomponents.mail }}
+              </p>
+            </div>
+          </div>
+          <div>
+            <h2 class="text-[#FFFFFF99]">
+              {{ FooterInfo[0].Footercomponents.media }}
+            </h2>
+            <div class="flex gap-3 pt-3">
+              <a href="#">
+              <div
+                class="flex justify-center items-center w-[48px] h-[48px] rounded-full bg-[#FFFFFF1A]"
+              >
+                <component
+                  class=""
+                  :is="FooterInfo[0].Footercomponents.mediaicon1"
+                />
+              </div>
+            </a>
+            <a href="#">
+              <div
+                class="flex justify-center items-center w-[48px] h-[48px] rounded-full bg-[#FFFFFF1A]"
+              >
+                <component
+                  class=""
+                  :is="FooterInfo[0].Footercomponents.mediaicon2"
+                />
+              </div>
+            </a>
+            <a href="#">
+              <div
+                class="flex justify-center items-center w-[48px] h-[48px] rounded-full bg-[#FFFFFF1A]"
+              >
+                <component
+                  class=""
+                  :is="FooterInfo[0].Footercomponents.mediaicon3"
+                />
+              </div>
+            </a>
+            <a href="#">
+              <div
+                class="flex justify-center items-center w-[48px] h-[48px] rounded-full bg-[#FFFFFF1A]"
+              >
+                <component
+                  class=""
+                  :is="FooterInfo[0].Footercomponents.mediaicon4"
+                />
+              </div>
+            </a>
+            </div>
+          </div>
         </div>
       </div>
       <hr
--- src/composables/useFooter.js
+++ src/composables/useFooter.js
@@ -1,6 +1,13 @@
 import { ref, markRaw } from "vue";
 import IsolationMode from "../assets/images/IsolationMode.png";
 import arrowRight from "../assets/icons/arrowRightPurle.vue";
+import mail from "../assets/icons/mail.vue";
+import phone from "../assets/icons/phone.vue";
+import address from "../assets/icons/address.vue";
+import youtube from "../assets/icons/youtube.vue";
+import Linked from "../assets/icons/in.vue";
+import facebook from "../assets/icons/facebook.vue";
+import instagram from "../assets/icons/instagram.vue";
 import { reactive } from "vue";
 
 export default (function useFooter() {
@@ -10,6 +17,20 @@ export default (function useFooter() {
         title: "Contact",
         img: IsolationMode,
         icon: markRaw(arrowRight),
+        icon1: markRaw(mail),
+        icon2: markRaw(phone),
+        icon3: markRaw(address),
+        title1: "Address",
+        title2: "Phone",
+        title3: "Email",
+        address: "Ilia Chavchavadze avenue N50, Tbilisi, Georgia",
+        number: "+995 592 35 45 65",
+        mail: "Info@otium.ge",
+        media: "Social media",
+        mediaicon1:markRaw(facebook),
+        mediaicon2:markRaw(instagram),
+        mediaicon3:markRaw(Linked),
+        mediaicon4:markRaw(youtube),
       },
     },
   ]);
