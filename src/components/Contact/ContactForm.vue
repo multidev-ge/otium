2127e251bd0a21ca0ba8a2de2173e28a16af7cfb
Gandobilis <lashadeveloper@gmail.com>
2023-08-21 15:38:48 +0400

    ContactInfo.vue Responsive design finished, page final version.

--- src/components/Contact/ContactForm.vue
+++ src/components/Contact/ContactForm.vue
@@ -6,7 +6,7 @@ const {contactFormData, submitContactForm} = useContact;
 </script>
 
 <template>
-  <form @submit.prevent="submitContactForm" class="space-y-7 font-medium">
+  <form @submit.prevent="submitContactForm" class="space-y-6 lg:space-y-7 font-medium">
     <input v-model="contactFormData.name" name="name" type="text" placeholder="Name"
            class="block w-full pl-5 py-3 border-2 border-opacity-30 rounded-md" required>
     <input v-model="contactFormData.phoneNumber" name="phone-number" type="tel" placeholder="Phone Number"
@@ -14,7 +14,7 @@ const {contactFormData, submitContactForm} = useContact;
     <textarea v-model="contactFormData.message" name="message" placeholder="Message"
               class="h-36 block w-full pl-5 pt-3 leading-6 border-2 border-opacity-30 resize-none rounded-md" required/>
     <button type="submit"
-            class="float-right w-36 flex items-center justify-center bg-black text-white px-6 py-3 rounded-2xl gap-x-1.5">
+            class="w-full md:w-36 flex items-center justify-center bg-black text-white px-6 py-3 rounded-2xl gap-x-1.5 lg:float-right">
       Send
       <ContactRightArrowIcon/>
     </button>
