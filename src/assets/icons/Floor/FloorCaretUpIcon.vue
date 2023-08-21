2f72049089138b39d8cda91277fc5054e84328b4
Gandobilis <lashadeveloper@gmail.com>
2023-08-21 18:12:07 +0400

    Floor Unresponsive Design Finished.

---
+++ src/assets/icons/Floor/FloorCaretUpIcon.vue
@@ -0,0 +1,5 @@
+<template>
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M12 8.62519L18 14.6252L16.925 15.7002L12 10.7502L7.075 15.6752L6 14.6002L12 8.62519Z" fill="#88407C"/>
+  </svg>
+</template>
