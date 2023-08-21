2a9070f65c05a8c6be82248f906020c11b4523d2
papunawereteli <papunawereteli20@gmail.com>
2023-08-21 21:25:46 +0400

    fixed

--- src/views/Pages/apartmentFinder.vue
+++ src/views/Pages/apartmentFinder.vue
@@ -1,11 +1,14 @@
 <script setup>
 import FiltersPart from "@/components/apartmentFinderPage/filtersPart.vue";
 import RightPart from "@/components/apartmentFinderPage/rightPart.vue";
+import MainLayout from "../../layouts/mainLayout.vue";
 </script>
 
 <template>
-<div class="flex container mx-auto justify-center gap-64">
-  <FiltersPart/>
-  <RightPart/>
-</div>
+<main-layout>
+  <div class="flex container mx-auto justify-center gap-64">
+    <FiltersPart/>
+    <RightPart/>
+  </div>
+</main-layout>
 </template>
