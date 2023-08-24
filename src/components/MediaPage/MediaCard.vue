ce8f3bc545f97f6380cd0de6ef55b63281f00ad1
KIU-PC <bibiashviliandria@gmail.com>
2023-08-24 19:20:29 +0400

    made media page responsive

--- src/components/MediaPage/MediaCard.vue
+++ src/components/MediaPage/MediaCard.vue
@@ -9,14 +9,23 @@ defineProps({
 
 <template>
   <div class="w-full">
-    <div class="h-44 w-full rounded-lg mb-7">
+    <div class="h-36 md:h-44 w-full rounded-lg mb-2 md:mb-7">
       <img alt="media img" :src="data.image" class="rounded-lg w-full h-full object-cover">
     </div>
-    <p class="text-lg font-medium text-[#626262] mb-2">
+    <p class="text-lg font-medium text-[#626262] md:mb-2">
       {{ data.category }}, {{ data.date }}
     </p>
-    <div class="h-28 text-3xl font-medium text-[#000] truncate-last-line">
+    <div class="h-28 text-xl md:text-3xl font-medium text-[#000] truncate-last-line">
       {{ data.title }}
     </div>
   </div>
 </template>
+
+<style scoped>
+.truncate-last-line {
+  display: -webkit-box;
+  -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+}
+</style>
