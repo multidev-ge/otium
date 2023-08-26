91ca882ca3219e1299f8827924482412b7983c7b
Gandobilis <lashadeveloper@gmail.com>
2023-08-26 21:41:44 +0400

    ContactPage Contact Info Component styling corrected.

--- src/components/Contact/ContactInfo.vue
+++ src/components/Contact/ContactInfo.vue
@@ -6,8 +6,8 @@ const {contactInfo} = useContact;
 </script>
 
 <template>
-  <div class="flex justify-between gap-x-9 max-lg:flex-col max-lg:gap-y-9">
-    <div v-for="(item, index) in contactInfo" :key="index" class="flex items-center gap-x-4 lg:gap-x-6 ">
+  <div class="flex max-lg:flex-col lg:justify-between lg:items-center lg:gap-x-9 max-lg:gap-y-9">
+    <div v-for="(item, index) in contactInfo" :key="index" class="flex items-start gap-x-4 lg:gap-x-6 ">
       <ContactItem :data="item"/>
     </div>
   </div>
