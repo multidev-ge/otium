978e3e83144ec62ba461641786093cbeb508ed53
Rikudo10 <giorgisamxaradze16@gmail.com>
2023-08-27 06:14:14 +0400

    Complete

---
+++ src/assets/icons/arrowleft.vue
@@ -0,0 +1,24 @@
+<template>
+  <svg
+    width="16"
+    height="16"
+    viewBox="0 0 16 16"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M13.5996 8L2.39961 8"
+      stroke="black"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M8 13.5996L2.4 7.99961L8 2.39961"
+      stroke="black"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
---
+++ src/composables/useTechnical.js
@@ -0,0 +1,93 @@
+import { ref } from "vue";
+import descriptionsvg from "../assets/icons/description.svg"
+import settingsvg from "../assets/icons/settings.svg"
+import foundationsvg from "../assets/icons/foundation.svg"
+import handyman from "../assets/icons/TechnicalCharacteristics/handyman.svg"
+import corporate from "../assets/icons/TechnicalCharacteristics/corporate.svg"
+import balcony from "../assets/icons/TechnicalCharacteristics/balcony.svg"
+import floor from "../assets/icons/TechnicalCharacteristics/floor.svg"
+import elevator from "../assets/icons/TechnicalCharacteristics/elevator.svg"
+import door from "../assets/icons/TechnicalCharacteristics/door.svg"
+import doorsliding from "../assets/icons/TechnicalCharacteristics/doorsliding.svg"
+import waterdrop from "../assets/icons/TechnicalCharacteristics/waterdrop.svg"
+import boltFILL0 from "../assets/icons/TechnicalCharacteristics/boltFILL0.svg"
+import layers from "../assets/icons/TechnicalCharacteristics/layers.svg"
+import homerepairservice from "../assets/icons/TechnicalCharacteristics/homerepairservice.svg"
+import spacedashboard from "../assets/icons/TechnicalCharacteristics/spacedashboard.svg"
+
+
+export default (function useTechnical() {
+    const technicalinfo = ref([
+        {
+                faqs : [
+                    {
+                      icon: descriptionsvg ,
+                      title: "Terms of delivery",
+                      active: false,
+                      faq: [
+                        {
+
+                        id: 0,
+                        title: "თეთრი კარკასი:",
+
+                        title1: "დასრულებული სამშენებლო-სამონტაჟო სამუშაოები",
+                        title2: "დასრულებული შენობის ფასადი",
+                        title3: "მოპირკეთებული აივნები დამონტაჟებული განათებით",
+                        title4: "დასრულებული სადარბაზო, კიბეები და კიბის უჯრედი",
+                        title5: "თანამედროვე ტიპის ლიფტები",
+                        title6: "საცხოვრებელი ბინის შესასვლელში ჩაყენებული რკინის კარი",
+                        title7: "მეტალოპლასტმასის კარ-ფანჯარა",
+                        title8: "წყლის და კანალიზაციის სისტემები შეყვანილი ბინაში (მონტაჟის წერტილამდე)",
+                        title9: "ბინაში შეყვანილი ელექტროენერგიის ძირითადი ხაზი მრიცხველით",
+                        title10: "მოჭიმული იატაკი (სველი წერტილების გარდა)",
+                        title11: "შელესილი შიდა კედლები",
+                        title12: "ამოყვანილი ბინის შიდა ტიხრები მყიდველთან წინასწარი შეთანხმებით ან/ და სამშენებლო პროექტის მიხედვით",
+
+                        icon1: handyman ,
+                        icon2: corporate ,
+                        icon3: balcony ,
+                        icon4: floor ,
+                        icon5: elevator ,
+                        icon6: door ,
+                        icon7: doorsliding ,
+                        icon8: waterdrop ,
+                        icon9: boltFILL0 ,
+                        icon10: layers ,
+                        icon11: homerepairservice ,
+                        icon12: spacedashboard ,
+                      },
+                      
+                    ]
+                    },
+                    {
+                      icon: foundationsvg ,
+                      title: "Construction information",
+                      active: false,
+                      faq: [
+                        {
+                         description: "Project Scope: Our construction project involves creating a modern commercial building with a focus on minimalistic design, energy efficiency, and sustainable practices.",
+                         description1: "Timeline: The construction timeline is estimated to be 12 months, starting from the groundbreaking ceremony. Materials: We prioritize eco-friendly and high-quality materials to ensure durability and minimize environmental impact. Our team selects materials that align with our minimalistic and sustainable approach. Budget: We have carefully planned the project's budget, and we will diligently manage expenses to ensure cost-effectiveness without compromising on quality.",
+                         description2: "Safety Measures: Safety is our top priority. We strictly adhere to industry safety standards and implement comprehensive safety protocols to protect our team, clients, and visitors.",
+                         description3: "Construction Progress: Throughout the project, we will provide regular updates on the construction progress. Our team will be available to address any queries or concerns you may have.",
+                         description4: "Quality Assurance: Our commitment to excellence drives us to maintain rigorous quality control measures, ensuring that the final result meets and exceeds your expectations. ",
+                         description5: "Sustainability: We actively incorporate sustainable practices to reduce our carbon footprint and create a positive impact on the environment. ",
+                         description6: "We are thrilled to work with you on this exciting construction journey. Should you have any questions or require further information, please do not hesitate to reach out to our team. Together, we will build a remarkable space that reflects our minimalist vision and your unique needs.",
+                        }
+                      ]
+                    },
+                    {
+                      icon: settingsvg ,
+                      title: "infrastructure",
+                      active: false,
+                    },  
+                  ]
+        }
+    ]);
+
+
+    let instance = { technicalinfo  };
+
+    return () => {
+        return instance;
+    };
+})();
