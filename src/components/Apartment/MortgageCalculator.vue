0a89c558f192a0350d049a14e7ffb08d4f80dc93
Gandobilis <lashadeveloper@gmail.com>
2023-08-30 17:47:50 +0400

    ApartmentPage Header Section Responsive Design Finished.

--- src/components/Apartment/MortgageCalculator.vue
+++ src/components/Apartment/MortgageCalculator.vue
@@ -5,39 +5,37 @@ import TBCLogo from "../../assets/logos/apartment/TBCLogo.png";
 </script>
 
 <template>
-  <div class="grid grid-cols-1 xl:grid-cols-2 bg-black rounded-xl">
-    <div class="flex flex-col justify-between p-4 xl:p-16">
-      <div class="flex flex-col gap-y-4">
-        <h2 class="text-white font-medium text-4.5xl">Calculator</h2>
-        <p class="text-white text-xl font-medium leading-8 opacity-60">
-          Fusce volutpat lectus nisl consectetur finibus scelerisque augue varius eros sapien diam, euismod pretium
-          id, volutpat tortor. In vulputate lorem quis
-        </p>
-      </div>
-      <div class="flex max-xl:flex-col max-xl:gap-y-5 items-center gap-x-6 max-xl:mt-7">
-        <a class="cursor-pointer" target="_blank"
-           href="https://bankofgeorgia.ge/ka/retail/loans/mortgage/mortgage-application">
-          <div class="xl:relative flex items-center py-4 xl:py-8 pl-10 xl:pl-6 pr-20 xl:pr-12 bg-[#FCE1D3] rounded-xl">
-            <div class="flex gap-x-4 items-center">
-              <img :src="BOGLogo" alt="BOG logo">
-              <span class="text-xl font-medium leading-8 whitespace-nowrap">Start applying</span>
-            </div>
-            <contact-right-arrow-icon stroke="black" class="-rotate-45 max-xl:ml-6 xl:absolute xl:top-3 xl:right-3"/>
-          </div>
-        </a>
-        <a class="cursor-pointer" target="_blank"
-           href="https://tbcmortgage.ge/?utm_source=google&utm_medium=search_keyword&utm_campaign=tbcmortgage_general&utm_content=mainkeywords&gad=1&gclid=Cj0KCQjwi7GnBhDXARIsAFLvH4mgkdxtROzPQkwxdd07hEBVsQJBPCsAL5xW9bHiCyBuNCK9gf8j-9UaAuc1EALw_wcB">
-          <div class="xl:relative flex  items-center py-4 xl:py-8 pl-10 xl:pl-6 pr-20 xl:pr-12 bg-[#E5F7FD] rounded-xl">
-            <div class="flex gap-x-4 items-center">
-              <img :src="TBCLogo" alt="TBC logo">
-              <span class="text-xl font-medium leading-8 whitespace-nowrap">Start applying</span>
-            </div>
-            <contact-right-arrow-icon stroke="black" class="-rotate-45 max-xl:ml-6 xl:absolute xl:top-3 xl:right-3"/>
-          </div>
-        </a>
-      </div>
+  <div class="flex max-xl:flex-col xl:grid xl:grid-cols-2 xl:grid-rows-2 bg-black rounded-xl">
+    <div class="order-1 flex flex-col gap-y-4 p-4 xl:p-16">
+      <h2 class="text-white font-medium text-4.5xl">Calculator</h2>
+      <p class="text-white text-xl font-medium leading-8 opacity-60">
+        Fusce volutpat lectus nisl consectetur finibus scelerisque augue varius eros sapien diam, euismod pretium
+        id, volutpat tortor. In vulputate lorem quis
+      </p>
     </div>
-    <div class="bg-white rounded-xl m-4 p-12 flex flex-col gap-y-12">
+    <div class="order-3 p-4 xl:p-16 flex max-xl:flex-col max-xl:gap-y-5 items-center gap-x-6 max-xl:mt-7">
+      <a class="max-sm:w-full cursor-pointer" target="_blank"
+         href="https://bankofgeorgia.ge/ka/retail/loans/mortgage/mortgage-application">
+        <div class="xl:relative flex items-center py-4 xl:py-8 pl-10 xl:pl-6 pr-20 xl:pr-12 bg-[#FCE1D3] rounded-xl">
+          <div class="flex gap-x-4 items-center">
+            <img :src="BOGLogo" alt="BOG logo">
+            <span class="text-xl font-medium leading-8 whitespace-nowrap">Start applying</span>
+          </div>
+          <contact-right-arrow-icon stroke="black" class="-rotate-45 max-xl:ml-6 xl:absolute xl:top-3 xl:right-3"/>
+        </div>
+      </a>
+      <a class="max-sm:w-full cursor-pointer" target="_blank"
+         href="https://tbcmortgage.ge/?utm_source=google&utm_medium=search_keyword&utm_campaign=tbcmortgage_general&utm_content=mainkeywords&gad=1&gclid=Cj0KCQjwi7GnBhDXARIsAFLvH4mgkdxtROzPQkwxdd07hEBVsQJBPCsAL5xW9bHiCyBuNCK9gf8j-9UaAuc1EALw_wcB">
+        <div class="xl:relative flex  items-center py-4 xl:py-8 pl-10 xl:pl-6 pr-20 xl:pr-12 bg-[#E5F7FD] rounded-xl">
+          <div class="flex gap-x-4 items-center">
+            <img :src="TBCLogo" alt="TBC logo">
+            <span class="text-xl font-medium leading-8 whitespace-nowrap">Start applying</span>
+          </div>
+          <contact-right-arrow-icon stroke="black" class="-rotate-45 max-xl:ml-6 xl:absolute xl:top-3 xl:right-3"/>
+        </div>
+      </a>
+    </div>
+    <div class="order-2 xl:row-span-2 bg-white rounded-xl m-4 p-12 flex flex-col gap-y-12">
       <div class="flex flex-col gap-y-4">
         <h3 class="text-2xl font-medium leading-8">
           Calculate mortgage
