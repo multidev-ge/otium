d531a7c643d0803cb522dd1a327feeffc487dfc7
papunawereteli <papunawereteli20@gmail.com>
2023-08-30 19:17:59 +0400

    male damtavrdeba

--- src/components/Projects/splideForProjects.vue
+++ src/components/Projects/splideForProjects.vue
@@ -10,6 +10,7 @@ import Projects from "@/composables/forProject/Projects";
 import RightArrow from "@/assets/icons/apartmentFinderPage/rightArrow.vue";
 import SplideArrow from "@/assets/icons/mediaInnerPage/splideArrow.vue";
 import ArrowForProjectsSplide from "@/assets/icons/Projects/arrowForProjectsSplide.vue";
+import {ref} from "vue";
 const projectsInfo = Projects
 const {projectsList} = projectsInfo()
 const mainOptions = {
@@ -18,24 +19,32 @@ const mainOptions = {
   pagination: false,
   focus: 'center',
   gap: '1rem',
-  arrows: false,
+  arrows: true,
   rewind: true,
   breakpoints: {
     768:{
-      heightRatio: 2,
+      heightRatio: 2.1,
       direction: 'ttb',
     }
   }
 };
+const currentValue = ref(1)
+const changeValue = (value) =>{
+  currentValue.value = value
+}
+
+
 </script>
 <template>
   <section class="">
-    <Splide class="w-full" :options="mainOptions" :has-track="false" >
-      <div class="custom-wrapper">
+    <Splide  :options="mainOptions" :has-track="false"  @splide:moved="function (newIndex, prevIndex, destIndex){
+      changeValue(newIndex.index+1)
+    }" >
+      <div class="custom-wrapper  flex flex-col container  w-11/12 mx-auto ">
         <SplideTrack>
-          <SplideSlide v-for="(project,index) in projectsList" @change="console.log('hey')">
-            <div>
-              <div class="container mx-auto flex md:flex-row flex-col md:w-full w-11/12 justify-between">
+          <SplideSlide v-for="(project,index) in projectsList">
+            <div class="flex ">
+              <div class=" flex md:flex-row flex-col md:w-full  justify-between">
                 <div class="md:w-2/6 flex flex-col md:items-start">
                   <p class="font-medium text-5xl md:text-8xl md:mb-12 mb-7">Projects</p>
                   <div class="gap-7 flex flex-col mb-6 ">
@@ -49,15 +58,30 @@ const mainOptions = {
                     </div>
                   </div>
                   <button class="bg-[#F0EEEC] flex items-center gap-3 px-6 py-3 justify-center  rounded-2xl mb-7 ">More About Project <right-arrow/></button>
-                  <div class="flex gap-7 relative items-center">
-                    <p class="text-xl">Scroll to view other projects</p>
-                  </div>
+
                 </div>
+
                 <img class="md:w-2/4 md:flex hidden" alt="" :src="project.image">
               </div>
             </div>
           </SplideSlide>
         </SplideTrack>
+
+
+        <div class="relative  md:w-1/4	flex  ">
+          <div class=" flex w-full relative items-center justify-between">
+
+            <p class="text-xl mr-5 md:w-auto w-1/2">Scroll to view other projects</p>
+
+            <div class="splide__arrows !flex relative  pt-24 ">
+              <button class="splide__arrow splide__arrow--prev !hidden">Prev</button>
+              <button class="splide__arrow !bg-black !w-12 !h-12 text-xl absolute   splide__arrow--next"><ArrowForProjectsSplide/></button>
+            </div>
+
+            <p class="md:-ml-16 md:pl-0 pl-8 text-2xl"><span>{{currentValue}}</span> / <span class="opacity-30">{{projectsList.length}}</span></p>
+          </div>
+
+        </div>
       </div>
     </Splide>
   </section>
