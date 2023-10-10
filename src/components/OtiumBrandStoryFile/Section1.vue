524981a44550d0b040f84e76e51c5245f78ad904
Rikudo10 <giorgisamxaradze16@gmail.com>
2023-10-10 19:13:06 +0400

    გვერდი კომპანია (brand history)

--- src/components/OtiumBrandStoryFile/Section1.vue
+++ src/components/OtiumBrandStoryFile/Section1.vue
@@ -6,11 +6,11 @@ const { BrandStory } = useOtiumBrandStory();
 <template>
   <div class="xl:flex   pb-14  ">
     <div class="  pb-6 xl:pt-0 xl:w-[63%]">
-      <h2 class="xl:text-8xl  text-5xl xl:gap-8  text-[#000000] font-ppNeueMontrealMedium md:pr-0 pr-[16%] ">{{ BrandStory[0].BrandStoryName.title }} <img class="xl:mt-16 inline w-[36px] md:w-auto	" src="../../assets/icons/forward.svg" alt="" /></h2>
+      <h2 class="xl:text-[100px] font-medium	leading-[120px]  text-5xl xl:gap-8  text-[#000000] font-ppNeueMontrealMedium md:pr-0 pr-[16%] ">{{ BrandStory[0].BrandStoryName.title }} <img class="xl:mt-16 inline w-[36px] md:w-auto	" src="../../assets/icons/forward.svg" alt="" /></h2>
 
     </div>
     <div class="xl:w-[415px]">
-      <h3 class="text-2xl	 md:text-3xl	   font-ppNeueMontrealMedium ">
+      <h3 class="text-2xl	 md:text-[28px]	font-medium	   font-ppNeueMontrealMedium ">
         {{ BrandStory[0].BrandStoryDescription.description5 }}
       </h3>
     </div>
@@ -23,17 +23,17 @@ const { BrandStory } = useOtiumBrandStory();
 </div>
 
   <div class="xl:flex xl:gap-8">
-    <p class="xl:w-[640px] pb-8 xl:pb-0 text-xl font-ppNeueMontrealMedium leading-8 pr-10 text-[#00000099]">
+    <p class="xl:w-[640px] pb-8 xl:pb-0 text-[22px] font-ppNeueMontrealMedium leading-8 pr-10 text-[#00000099]">
       {{ BrandStory[0].BrandStoryDescription.description }}
     </p>
-    <p class="xl:w-[640px] text-xl font-ppNeueMontrealMedium leading-8 text-[#00000099]">
+    <p class="xl:w-[640px] text-[22px] font-ppNeueMontrealMedium leading-8 text-[#00000099] ">
       {{ BrandStory[0].BrandStoryDescription.description1 }}
     </p>
   </div>
   <div class=" ">
     <div class="lg:pt-24 pt-12  flex md:gap-8 gap-4 lg:pb-20 pb-7 xl:w-[700px]">
-      <h2 class="md:text-8xl text-3xl	 font-ppNeueMontrealMedium">{{ BrandStory[0].BrandStoryName.title1 }}</h2>
-      <img class="lg:pt-10 w-[36px] h-[36px] md:w-auto md:h-auto" src="../../assets//icons/forward.svg" alt="" />
+      <h2 class="md:text-[100px]  font-medium	leading-[120px] text-3xl	 font-ppNeueMontrealMedium">{{ BrandStory[0].BrandStoryName.title1 }}</h2>
+      <img class="lg:pt-4 w-[36px] h-[36px] md:w-auto md:h-auto" src="../../assets//icons/forward.svg" alt="" />
     </div>
 
   </div>
