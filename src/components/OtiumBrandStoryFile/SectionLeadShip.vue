0f1b254a2eb5131b565c87fd747f8f875100dfd7
Rikudo10 <giorgisamxaradze16@gmail.com>
2023-10-10 19:00:20 +0400

    გვერდი კომპანია (brand history) ქარდები

--- src/components/OtiumBrandStoryFile/SectionLeadShip.vue
+++ src/components/OtiumBrandStoryFile/SectionLeadShip.vue
@@ -3,60 +3,60 @@
     <div class="lg:flex gap-8 ">
       <div class="pb-7 lg:pb-0">
         <img
-          class="rounded-lg"
+          class="rounded-lg w-[304px] h-[303px]"
           :src="BrandStory[0].BrandStoryPhotos.img1"
           alt="Slide Image"
         />
-        <h2 class="pt-2 font-ppNeueMontrealMedium text-2xl grid justify-start">
+        <h2 class="pt-2 font-ppNeueMontrealMedium text-2xl grid justify-start font-medium	">
           {{ BrandStory[0].BrandStoryName.name }}
         </h2>
         <p
-          class="pt-1 lg:pt-2 text-xl font-ppNeueMontrealMedium text-[#00000099] grid justify-start"
+          class="pt-1 lg:pt-2 text-[22px] font-ppNeueMontrealMedium text-[#00000099] grid justify-start"
         >
           {{ BrandStory[0].BrandStoryName.position1 }}
         </p>
       </div>
       <div class="pb-7 lg:pb-0">
         <img
-          class="rounded-lg"
+          class="rounded-lg w-[304px] h-[303px]"
           :src="BrandStory[0].BrandStoryPhotos.img2"
           alt="Slide Image"
         />
-        <h2 class="pt-2 font-ppNeueMontrealMedium text-2xl grid justify-start">
+        <h2 class="pt-2 font-ppNeueMontrealMedium text-2xl grid justify-start font-medium">
           {{ BrandStory[0].BrandStoryName.name }}
         </h2>
         <p
-          class="pt-1 lg:pt-2 text-xl font-ppNeueMontrealMedium text-[#00000099] grid justify-start"
+          class="pt-1 lg:pt-2 text-[22px]  font-ppNeueMontrealMedium text-[#00000099] grid justify-start"
         >
           {{ BrandStory[0].BrandStoryName.position1 }}
         </p>
       </div>
       <div class="pb-7 lg:pb-0">
         <img
-          class="rounded-lg"
+          class="rounded-lg w-[304px] h-[303px]"
           :src="BrandStory[0].BrandStoryPhotos.img3"
           alt="Slide Image"
         />
-        <h2 class="pt-2 font-ppNeueMontrealMedium text-2xl grid justify-start">
+        <h2 class="pt-2 font-ppNeueMontrealMedium text-2xl grid justify-start font-medium">
           {{ BrandStory[0].BrandStoryName.name }}
         </h2>
         <p
-          class="pt-1 lg:pt-2 text-xl font-ppNeueMontrealMedium text-[#00000099] grid justify-start"
+          class="pt-1 lg:pt-2 text-[22px] font-ppNeueMontrealMedium text-[#00000099] grid justify-start"
         >
           {{ BrandStory[0].BrandStoryName.position2 }}
         </p>
       </div>
       <div class="pb-7 lg:pb-0">
         <img
-          class="rounded-lg"
+          class="rounded-lg w-[304px] h-[303px]"
           :src="BrandStory[0].BrandStoryPhotos.img4"
           alt="Slide Image"
         />
-        <h2 class="pt-2 font-ppNeueMontrealMedium text-2xl grid justify-start">
+        <h2 class="pt-2 font-ppNeueMontrealMedium text-2xl grid justify-start font-medium">
           {{ BrandStory[0].BrandStoryName.name }}
         </h2>
         <p
-          class="pt-1 lg:pt-2text-xl font-ppNeueMontrealMedium text-[#00000099] grid justify-start"
+          class="pt-1 lg:pt-2 text-[22px] font-ppNeueMontrealMedium text-[#00000099] grid justify-start"
         >
           {{ BrandStory[0].BrandStoryName.position2 }}
         </p>
