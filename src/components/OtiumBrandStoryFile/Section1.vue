c18f3649a524f61f94d3ccbdbccdcd53f8815cab
Rikudo10 <giorgisamxaradze16@gmail.com>
2023-10-10 21:37:01 +0400

    Otium brand story fix

--- src/components/OtiumBrandStoryFile/Section1.vue
+++ src/components/OtiumBrandStoryFile/Section1.vue
@@ -5,8 +5,9 @@ const { BrandStory } = useOtiumBrandStory();
 </script>
 <template>
   <div class="xl:flex   xl:pb-14 pb-[27px] ">
-    <div class="  pb-6 xl:pt-0 xl:w-[63%]">
-      <h2 class="xl:text-[100px] font-medium	xl:leading-[120px] leading-[57px]  text-5xl xl:gap-8  text-[#000000] font-ppNeueMontrealMedium md:pr-0 pr-[16%] ">{{ BrandStory[0].BrandStoryName.title }} <img class="xl:mt-16 inline w-[36px] md:w-auto	" src="../../assets/icons/forward.svg" alt="" /></h2>
+    <div class="pb-6 xl:pt-0 xl:w-[63%] ">
+      <h2 class="xl:text-[100px] font-medium	xl:leading-[120px] leading-[57px]  text-5xl xl:gap-8  text-[#000000] font-ppNeueMontrealMedium md:pr-0 pr-[16%] ">{{ BrandStory[0].BrandStoryName.title }} 
+        <img class="xl:mt-14 inline w-[36px] md:w-auto	xl:pl-[32px]" src="../../assets/icons/forward.svg" alt="" /></h2>
 
     </div>
     <div class="xl:w-[415px]">
