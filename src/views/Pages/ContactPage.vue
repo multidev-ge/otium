88c6480934ea0a48aefa385f0f8893f23207ab96
Gandobilis <lashadeveloper@gmail.com>
2023-10-11 11:46:53 +0400

    ContactPage title sizes corrected.

--- src/views/Pages/ContactPage.vue
+++ src/views/Pages/ContactPage.vue
@@ -11,13 +11,13 @@ import MainLayout from "@/layouts/mainLayout.vue"
     <div
         class="flex flex-col gap-y-8 lg:gap-y-16">
       <h1
-          class="font-medium text-5xl sm:max-2xl:text-6.5xl 2xl:text-8xl leading-normal">
+          class="text-5xl font-medium leading-[57.6px] md:text-[100px] md:leading-[120px]">
         Contact
       </h1>
 
       <contact-info/>
 
-      <div class="grid grid-cols-1 lg:grid-cols-2 max-lg:gap-y-20 lg:gap-x-36">
+      <div class="grid grid-cols-1 max-lg:gap-y-20 lg:grid-cols-2 lg:gap-x-36">
         <contact-form/>
         <contact-map class="h-56 sm:max-md:h-64 md:max-lg:h-80 lg:h-full"/>
       </div>
