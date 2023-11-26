1c7ca632f95b7eecee8596a2d80340ef185b84e5
MCHEKHASHVILI <m.chekhashvili@gmail.com>
2023-11-27 00:09:58 +0400

    Both blocks are maped

--- src/components/Headings/HomePageTitle.vue
+++ src/components/Headings/HomePageTitle.vue
@@ -4,7 +4,7 @@ defineProps({
 })
 </script>
 <template>
-    <h1 class="my-6 py-6">
+    <h1 class="mb-6 pb-6">
         <span class="text-start font-medium text-5xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl">
             {{ title }}
         </span>
