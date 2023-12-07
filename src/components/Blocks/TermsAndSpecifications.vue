6107877d5103cfa7904b4f01742536cdebe2fdf3
MCHEKHASHVILI <m.chekhashvili@gmail.com>
2023-12-08 02:05:24 +0400

    slight changes according to levan's messages

---
+++ src/components/Blocks/TermsAndSpecifications.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { useRoute } from 'vue-router'
+import { useI18n } from "vue-i18n";
+defineProps(['content'])
+const route = useRoute()
+const { t } = useI18n({ useScope: 'global' })
+</script>
+<template>
+    <div class="w-full flex flex-col lg:flex-row">
+        <div class="lg:w-1/3 flex justify-center items-center mb-5 lg:mb-0">
+            <h3 class="text-4xl font-medium">{{ content?.title }}</h3>
+        </div>
+        <div class="flex flex-col lg:flex-row  lg:w-2/3 p-6 border border-1 border-[#F0EEEC] rounded-xl">
+            <div class="w-full text-[#626262] text-lg font-medium mb-2 lg:mb-0">{{ content?.description }}</div>
+            <div class="flex flex-col gap-5">
+                <router-link :to="`/projects/${route.params?.id}/details`">
+                    <button class="lg:w-60 w-full bg-[#F0EEEC] rounded-2xl p-3">
+                        <p>{{ t('buttons.termsOfDelivery') }}
+                            <right-arrow class="inline-block ml-1" />
+                        </p>
+                    </button>
+                </router-link>
+            </div>
+        </div>
+    </div>
+</template>
