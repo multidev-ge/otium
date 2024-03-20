49db13b3cc73c3ed077b934c2d4fe9a667b65839
MCHEKHASHVILI <m.chekhashvili@gmail.com>
2024-03-21 00:12:02 +0400

    design changes

--- src/components/Blocks/WhatWeOffer.vue
+++ src/components/Blocks/WhatWeOffer.vue
@@ -22,8 +22,8 @@ defineProps<{
     <div class="lg:w-3/4 flex lg:justify-center">
       <div class="grid grid-cols-1 lg:grid-cols-2 gap-y-6 md:gap-y-20 gap-x-28">
         <div v-for="item in content.boxes" class="flex flex-col max-w-sm">
-          <div class=" flex justify-center items-center w-16 h-16 bg-[#F2F2F2] rounded-xl mb-5">
-            <img :src="`https://dashboard.otium.ge/uploads/${item.icon}`" />
+          <div class=" flex justify-center items-center w-16 h-16 bg-[#923B80] rounded-xl mb-5">
+            <img :src="`https://dashboard.otium.ge/uploads/${item.icon}`" class="invert" />
           </div>
           <h4 class="block  text-2xl text-[#000000] mb-2">{{ item.title }}</h4>
           <p class="block text-lg text-[#626262] font-medium">
